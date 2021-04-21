fb9a0c30c5f6076ffa039a89daa0c0ee638607b1
MeTrance <acmepc12@gmail.com>
2021-04-21 14:34:45 +0300

    Time/Date/Week Fix

---
+++ database/migrations/2021_04_19_190016_create_tables_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTablesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tables', function (Blueprint $table) {
+            $table->id('id');
+            $table->date('ziņojuma_datums');
+            $table->time('laiks');
+            $table->char('atskaitošā_persona');
+            $table->char('avots');
+            $table->char('ziņojuma_apraksts');
+            $table->char('atrašanās_vieta');
+            $table->char('ierīces_tips');
+            $table->char('problēmas_veids');
+            $table->char('piezīmes');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tables');
+    }
+}
